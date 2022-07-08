219be6ac741132a828236ad677fd4484ba09fc3b
Elif Özceylan <elifozceylan@nara.com.tr>
2022-07-08 14:38:12 +0300

    update and gif addition

---
+++ Bloch Sphere.py
@@ -0,0 +1,17 @@
+
+pip install bloch_sphere
+
+from bloch_sphere.animate_bloch import do_or_save_animation, AnimState
+
+@do_or_save_animation('my_animation', save=False, fps=20, preview=True)
+# Or
+#@do_or_save_animation('my_animation', save='gif', fps=20, preview=True)
+#@do_or_save_animation('my_animation', save='mp4', fps=20, preview=False)
+def animate(state: AnimState):
+    state.x_gate()
+    state.y_gate()
+    state.z_gate()
+    state.h_gate()
+    state.s_gate()
+    ...
+    state.wait()  # Pause at the end
